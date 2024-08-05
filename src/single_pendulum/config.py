ff68b2f3e9f9bdb26e522f7c2eaa4a061ef44036
Yuri Paglierani <57718806+YuriPaglierani@users.noreply.github.com>
2024-08-05 03:31:27 +0200

    Add files via upload

---
+++ src/single_pendulum/config.py
@@ -0,0 +1,28 @@
+single_pendulum_config = {
+    "mass": 1.0,
+    "length": 1.0,
+    "g": 3,
+    "num_trajectories": 100,
+    "trajectory_length": 120,
+    "dt": 0.2 * (3./15),
+    'theta_min': -2.0,
+    'theta_max': +2.0,
+    'p_min': -5.0,
+    'p_max': 5.0,
+    'noise_std': 0.01
+}
+
+single_pendulum_training = {
+    "dt": 0.2 * (3./15),
+    "num_epochs": 400,
+    "batch_size": 128,
+    "learning_rate": 0.001,
+    "hidden_dim": 200,
+    "num_layers": 3,
+    "scheduler_factor": 0.9,
+    "scheduler_patience": 10,
+    "train_split": 0.8,
+    "val_split": 0.1,
+    "test_split": 0.1,
+    "seed": 42
+}
